c717e76e510001bea4f4f20927f88cdc409fdb9d
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-04-05 13:51:19 +0600

    some changed

--- src/Components/SingleReview/SingleReview.js
+++ src/Components/SingleReview/SingleReview.js
@@ -1,14 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './SingleReview.css'
 
 const SingleReview = (props) => {
-    const {name, img, ratings} = props.getReviews;
+    const {name, img, ratings, details} = props.getReviews;
      
   return (
     <div className='review'>
       <img src={img} alt="" />
       <h4>Name:{name}</h4>
-      <p><small>Ratings:{ratings}</small></p>
+      <p><small>{details}</small></p>
+      <p><small>Ratings:{ratings} <FontAwesomeIcon  className='fnt-aws' icon="fa-solid fa-star-half-stroke" /></small></p>
     </div>
   );
 };
